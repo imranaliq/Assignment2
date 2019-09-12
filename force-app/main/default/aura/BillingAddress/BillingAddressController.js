1ff5a8e038bd48ca5472487ade39237d0c586c55
imran ali <imranali_06@yahoo.com>
2019-09-12 19:32:55 +0500

    changes

--- force-app/main/default/aura/BillingAddress/BillingAddressController.js
+++ force-app/main/default/aura/BillingAddress/BillingAddressController.js
@@ -1,8 +1,6 @@
 ({
     clickCreate: function (cmp, event, helper) {
 
-        // Create the new expense
-        alert('test');
         var validAddress = cmp.find('billingform').reduce(function (validSoFar, inputCmp) {
             inputCmp.showHelpMessageIfInvalid();
             return validSoFar && inputCmp.get('v.validity').valid;
